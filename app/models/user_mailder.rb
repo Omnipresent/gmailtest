900dc83cee652ad65155bef9498ebd2150ced07a
Bhaarat <bhaarat.s@gmail.com>
2010-07-27 22:41:17 -0400

    change

--- app/models/user_mailder.rb
+++ app/models/user_mailder.rb
@@ -4,7 +4,7 @@ class UserMailder < ActionMailer::Base
     from       "My Forum "
     subject    "Please activate your new account"
     sent_on    Time.now
-    body       { :user => user, :url => activate_url(user.activation_code, :host => user.site.host }
+    body       { :user => "aasdfa", :url => "asdfd", :host => "asdfasdf" }
   end  
 end
 
